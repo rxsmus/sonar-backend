0cefadb7cf0a85cf7dbc254526faccf9171bcd4d
Rasmus <leinsalurasmus@gmail.com>
2025-10-14 20:25:37 +0200

    Match users by currently playing song

--- presence-server.js
+++ presence-server.js
@@ -16,22 +16,47 @@ const io = new Server(server, {
   }
 });
 
+// onlineUsers: { socketId: { username, songId } }
 let onlineUsers = {};
 
 io.on('connection', (socket) => {
   let username = null;
+  let songId = null;
 
-  socket.on('join', (name) => {
-    username = name;
-    onlineUsers[socket.id] = username;
-    io.emit('online-users', Object.values(onlineUsers));
+  // Expect join payload: { username, songId }
+  socket.on('join', (payload) => {
+    username = payload.username;
+    songId = payload.songId;
+    onlineUsers[socket.id] = { username, songId };
+    broadcastUsersForSong(songId);
+  });
+
+  socket.on('update-song', (newSongId) => {
+    if (onlineUsers[socket.id]) {
+      onlineUsers[socket.id].songId = newSongId;
+      songId = newSongId;
+      broadcastUsersForSong(songId);
+    }
   });
 
   socket.on('disconnect', () => {
     delete onlineUsers[socket.id];
-    io.emit('online-users', Object.values(onlineUsers));
+    if (songId) broadcastUsersForSong(songId);
   });
 });
+
+function broadcastUsersForSong(songId) {
+  // Only users with the same songId
+  const users = Object.values(onlineUsers)
+    .filter(u => u.songId === songId)
+    .map(u => u.username);
+  // Send only to sockets with this songId
+  Object.entries(onlineUsers).forEach(([sid, u]) => {
+    if (u.songId === songId && io.sockets.sockets.get(sid)) {
+      io.sockets.sockets.get(sid).emit('online-users', users);
+    }
+  });
+}
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
