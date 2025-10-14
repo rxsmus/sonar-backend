43ab3b31a252554271e00d0afb85c0274e5e16d6
Rasmus <leinsalurasmus@gmail.com>
2025-10-14 19:56:15 +0200

    Add Express health check route for Render

--- presence-server.js
+++ presence-server.js
@@ -1,7 +1,14 @@
-const { Server } = require('socket.io');
-const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+
+app.get('/', (req, res) => {
+  res.send('Presence server is running');
+});
+
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
