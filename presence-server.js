8b214b810b031894c87cf4121fcc3a56799b0059
Rasmus <leinsalurasmus@gmail.com>
2025-10-16 18:58:33 +0200

    implementing chat

--- presence-server.js
+++ presence-server.js
@@ -17,11 +17,13 @@ const io = new Server(server, {
   }
 });
 
+
 // Dynamic namespaces for /lobby/:trackId
 io.of(/^\/lobby\/.+$/).on('connection', (socket) => {
   const nsp = socket.nsp;
-  // Each namespace manages its own online users
+  // Each namespace manages its own online users and chat history
   if (!nsp.onlineUsers) nsp.onlineUsers = {};
+  if (!nsp.chatHistory) nsp.chatHistory = [];
   let username = null;
   let songId = nsp.name.split('/').pop();
 
@@ -29,6 +31,20 @@ io.of(/^\/lobby\/.+$/).on('connection', (socket) => {
     username = payload.username;
     nsp.onlineUsers[socket.id] = { username };
     broadcastUsers(nsp);
+    // Send chat history to the newly joined user
+    socket.emit('chat-history', nsp.chatHistory);
+  });
+
+  socket.on('send-message', (msg) => {
+    // msg: { user, message, timestamp, avatar }
+    const chatMsg = {
+      ...msg,
+      id: Date.now() + '-' + Math.random().toString(36).slice(2, 8)
+    };
+    nsp.chatHistory.push(chatMsg);
+    // Limit chat history to last 100 messages
+    if (nsp.chatHistory.length > 100) nsp.chatHistory = nsp.chatHistory.slice(-100);
+    nsp.emit('new-message', chatMsg);
   });
 
   socket.on('disconnect', () => {
